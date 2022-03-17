122c7a24f902fd0d5f4a556c5633ff7734b2220b
Samir Gonzalez <sekesie2@gmail.com>
2022-03-16 21:42:09 -0500

    sad

---
+++ Guineo/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace Guineo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(52, 98);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateSelected += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateSelected);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(312, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(404, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Create Task";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Location = new System.Drawing.Point(388, 93);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(352, 300);
+            this.panel1.TabIndex = 4;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(91, 30);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 17);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "DATEE";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 30);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Date";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(91, 128);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(237, 75);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Details";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Title of Task";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(91, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(237, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(40, 248);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 27);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Cancel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.button3.Location = new System.Drawing.Point(229, 248);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 27);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Confirm";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 40);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "TASK CALENDAR";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(388, 123);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(352, 270);
+            this.panel2.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(309, 194);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(209, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Fecha";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "AVAILABLE TASK FOR:";
+            // 
+            // button5
+            // 
+            this.button5.ForeColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(124, 234);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 27);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Delete";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Guineo/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Guineo
+{
+    public partial class Form1 : Form
+    {
+        OleDbDataAdapter ds;
+        private BindingSource bindingSource = null;
+        private OleDbCommandBuilder oleCommandBuilder = null;
+        DataTable dataTable = new DataTable();
+        public string selecteddate;
+
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Hide();
+            panel2.Hide();
+            
+        }
+
+        private void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            panel1.Hide();
+            panel2.Show();
+            label5.Text = monthCalendar1.SelectionRange.Start.ToShortDateString();
+            refreshgrid();
+            string selecteddate = monthCalendar1.SelectionRange.Start.ToShortDateString();
+            label8.Text = selecteddate; 
+            Console.WriteLine(selecteddate);
+        }
+
+        private void searchdb(string search)
+        {
+            try
+            {
+                dataGridView1.DataSource = null;
+                dataTable.Clear(); 
+                OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Internal.accdb");
+                con.Open();
+                ds = new OleDbDataAdapter("SELECT Task_ID, task_title, details, fecha FROM Tasks WHERE task_title LIKE '%" + search + "%' OR details LIKE '%" + search + "%'", con); //Query to display the wanted columns in the DB
+                oleCommandBuilder = new OleDbCommandBuilder(ds);
+                ds.Fill(dataTable);
+                bindingSource = new BindingSource { DataSource = dataTable };
+                dataGridView1.DataSource = bindingSource;
+                con.Close();
+                dataGridView1.RowHeadersVisible = false;
+                dataGridView1.Columns[0].HeaderText = "ID"; 
+                dataGridView1.Columns[1].HeaderText = "Title";
+                dataGridView1.Columns[2].HeaderText = "Details";
+                dataGridView1.Columns[3].HeaderText = "Date";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+
+        private void refreshgrid()
+        {
+            try
+            {
+                dataGridView1.DataSource = null;
+                dataTable.Clear(); //Clears the datatable for refreshing any changes
+                OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Internal.accdb");
+                con.Open();
+                ds = new OleDbDataAdapter("SELECT Task_ID, task_title, details FROM Tasks WHERE fecha LIKE '%" + monthCalendar1.SelectionRange.Start.ToShortDateString() + "%'", con); //Query to display the wanted columns in the DB
+                oleCommandBuilder = new OleDbCommandBuilder(ds);
+                ds.Fill(dataTable);
+                bindingSource = new BindingSource { DataSource = dataTable };// Binds any changes done in the datagrid with the database
+                dataGridView1.DataSource = bindingSource;
+                con.Close();
+                dataGridView1.RowHeadersVisible = false;
+                dataGridView1.Columns[0].HeaderText = "ID"; //Setting the names for the columns in the DataGrid
+                dataGridView1.Columns[1].HeaderText = "Title";
+                dataGridView1.Columns[2].HeaderText = "Details";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            panel1.Hide();
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+        
+        //search button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Hide();
+            panel2.Show();
+            searchdb(textBox1.Text);
+
+        }
+
+        //Create task
+        private void button2_Click(object sender, EventArgs e)
+        {
+          panel2.Hide();
+          panel1.Show();
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+         
+
+            if (textBox2.Text != "" && textBox3.Text != "")
+            {
+                try
+                {
+                    OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Internal.accdb");
+                    con.Open();
+
+                    Console.WriteLine("INSERT INTO Tasks(task_title, details, fecha) VALUES(@param1, @param2, @param3)");
+
+
+                    OleDbCommand cmd = new OleDbCommand("INSERT INTO Tasks (task_title, details, fecha) VALUES(@param1,@param2,@param3)", con);
+                    cmd.Parameters.AddWithValue("@param1", textBox2.Text);
+                    cmd.Parameters.AddWithValue("@param2", textBox3.Text);
+                    cmd.Parameters.AddWithValue("@param3", monthCalendar1.SelectionRange.Start.ToShortDateString());
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    textBox2.Clear();
+                    textBox3.Clear();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                DialogResult dialogResult = MessageBox.Show("You are about to delete a task permanently from the Database, are you sure?", "WARNING", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+
+                    //Gets the index from the selected row in the datagrid.
+                    int selectedIndex = dataGridView1.CurrentCell.RowIndex;
+
+                    //Turns it into a UserID, so the information can be deleted in the database.
+                    int rowID = Convert.ToInt32(dataGridView1.CurrentCell.RowIndex);
+                    try
+                    {
+
+                        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Internal.accdb");
+                        con.Open();
+                        OleDbCommand cmd = new OleDbCommand("DELETE FROM Tasks WHERE fecha LIKE '%" + monthCalendar1.SelectionRange.Start.ToShortDateString() + "%'", con);
+                        cmd.ExecuteNonQuery();
+                        Console.WriteLine("Deletion attempted");
+                        con.Close();
+                        MessageBox.Show("deleted");
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    //Deletion of the row in the dataGridView
+                    dataGridView1.Rows.RemoveAt(selectedIndex);
+
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    //do something else
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+
+
+        }
+    }
+}
